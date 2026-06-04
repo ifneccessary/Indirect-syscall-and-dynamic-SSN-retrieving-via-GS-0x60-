0d495878ea90ea87aa189118fd40940a83978fec
ifnecessary <235175137+ifnecessary@users.noreply.github.com>
2026-06-04 20:10:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ nasm -f win64 syscall.asm -o syscall.o
 
 x86_64-w64-mingw32-gcc main.c -I. -c -o main.o
 
-x86_64-w64-mingw32-gcc main.o syscall.o -o guduna.exe
+x86_64-w64-mingw32-gcc main.o syscall.o -o malware.exe
 
